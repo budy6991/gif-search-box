0d9e1abc3a5572ff090432d7c3d0a3c40563d60d
budy6991 <alvmenfer@gmail.com>
2022-07-27 14:03:43 +0200

    Fixed style, added readme file, to implement catch

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # gif-search-box
+
+Simple API test
+
+It allows you to search for random gifs online
+
+You can have a look at the result <a href="https://budy6991.github.io/form-validation/">here</a>
--- script.js
+++ script.js
@@ -1,26 +1,24 @@
-const img = document.querySelector('img')
-const searchBox = document.getElementById('gifSearch')
+const img = document.querySelector('img');
+const searchBox = document.getElementById('gifSearch');
 
-function refreshPage(){
-    window.location.reload();
-} 
+function refreshPage() {
+  window.location.reload();
+}
 
-fetch('https://api.giphy.com/v1/gifs/translate?api_key=jWd7HkgR1P1A2dOxpycwD4RHJMtgwzMf&s=cats', {mode : 'cors'})
+fetch('https://api.giphy.com/v1/gifs/translate?api_key=jWd7HkgR1P1A2dOxpycwD4RHJMtgwzMf&s=cats', { mode: 'cors' })
 
-.then((response) => response.json())
+  .then((response) => response.json())
 
-.then((response) => img.src = response.data.images.original.url) 
+  .then((response) => img.src = response.data.images.original.url);
 
 document.querySelector('.search').onclick = () => {
-    if (searchBox.value === '') {
-       location.reload()
-    }
+  if (searchBox.value === '') {
+    location.reload();
+  } else if (searchBox.value != '') {
+    fetch(`https://api.giphy.com/v1/gifs/translate?api_key=jWd7HkgR1P1A2dOxpycwD4RHJMtgwzMf&s=${searchBox.value}`, { mode: 'cors' })
 
-    else if (searchBox.value != '') {
-        fetch(`https://api.giphy.com/v1/gifs/translate?api_key=jWd7HkgR1P1A2dOxpycwD4RHJMtgwzMf&s=${searchBox.value}`, {mode : 'cors'})
+      .then((response) => response.json())
 
-.then((response) => response.json())
-
-.then((response) => img.src = response.data.images.original.url) 
-    }
-}
+      .then((response) => img.src = response.data.images.original.url);
+  }
+};
