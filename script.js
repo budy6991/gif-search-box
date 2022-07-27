ec5fc3b670e9707d1ce9c97fd8440fc22591f646
budy6991 <alvmenfer@gmail.com>
2022-07-27 13:49:29 +0200

    Search box working

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 const img = document.querySelector('img')
-fetch('https://api.giphy.com/v1/gifs/translate?api_key=jWd7HkgR1P1A2dOxpycwD4RHJMtgwzMf&s=spain', {mode : 'cors'})
+const searchBox = document.getElementById('gifSearch')
+
+function refreshPage(){
+    window.location.reload();
+} 
+
+fetch('https://api.giphy.com/v1/gifs/translate?api_key=jWd7HkgR1P1A2dOxpycwD4RHJMtgwzMf&s=cats', {mode : 'cors'})
 
 .then((response) => response.json())
 
 .then((response) => img.src = response.data.images.original.url) 
+
+document.querySelector('.search').onclick = () => {
+    if (searchBox.value === '') {
+       location.reload()
+    }
+
+    else if (searchBox.value != '') {
+        fetch(`https://api.giphy.com/v1/gifs/translate?api_key=jWd7HkgR1P1A2dOxpycwD4RHJMtgwzMf&s=${searchBox.value}`, {mode : 'cors'})
+
+.then((response) => response.json())
+
+.then((response) => img.src = response.data.images.original.url) 
+    }
+}
