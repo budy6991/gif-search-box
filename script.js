f409545cbae5c2e394fe447d1be4c607fb985aa7
budy6991 <alvmenfer@gmail.com>
2022-07-28 13:16:17 +0200

    Final version, code linted

--- script.js
+++ script.js
@@ -4,17 +4,19 @@ const errorContainer = document.getElementById('error-container');
 
 const makeFetchRequest = async () => {
   searchparam = userInput.value !== '' ? userInput.value : 'cats';
+  try {
   const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=jWd7HkgR1P1A2dOxpycwD4RHJMtgwzMf&s=${searchparam}`, { mode: 'cors' });
   const responseData = await response.json();
   img.src = responseData.data.images.original.url;
   errorContainer.textContent = '';
+  } catch(err) {
+    errorContainer.textContent = 'Ups something went wrong, try again';
+    err.textContent = errorContainer.textContent;
+  }
 };
 
 document.querySelector('.search').onclick = () => {
-  makeFetchRequest().catch((err) => {
-    errorContainer.textContent = 'Ups something went wrong, try again';
-    err.textContent = errorContainer.textContent;
-  });
+  makeFetchRequest()
 };
 
 makeFetchRequest();
