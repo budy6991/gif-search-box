4bd785228832c683a44aa8d29927adb1414cb43a
budy6991 <alvmenfer@gmail.com>
2022-07-28 11:43:32 +0200

    Added catch method with customized error message that spans

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const img = document.querySelector('img');
-const userInput = document.getElementById('gifSearch');
+const userInput = document.getElementById('userInput');
+const errorContainer = document.getElementById('error-container')
 
 async function getCats() {
   const response = await fetch('https://api.giphy.com/v1/gifs/translate?api_key=jWd7HkgR1P1A2dOxpycwD4RHJMtgwzMf&s=cats', { mode: 'cors' })
@@ -7,19 +8,22 @@ async function getCats() {
   img.src = catData.data.images.original.url;
 }
 
-getCats()
+async function getUserInput() {
+  const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=jWd7HkgR1P1A2dOxpycwD4RHJMtgwzMf&s=${userInput.value}`, { mode: 'cors' })
+    const responseData = await response.json()
+    img.src = responseData.data.images.original.url
+}
 
 document.querySelector('.search').onclick = () => {
   if (userInput.value === '') {
     location.reload();
   } else if (userInput.value != '') {
-    fetch(`https://api.giphy.com/v1/gifs/translate?api_key=jWd7HkgR1P1A2dOxpycwD4RHJMtgwzMf&s=${userInput.value}`, { mode: 'cors' })
-    .then(function(response) {
-      return response.json()
+    getUserInput().catch(err=> {
+      err.textContent = 'Ups! Something went wrong!'
+      errorContainer.textContent = err.textContent
+      location.reload()
     })
-    .then(function(response) {
-      img.src = response.data.images.original.url
-    }).catch(e => console.error(e)
-    )
   }
 };
+
+getCats()
